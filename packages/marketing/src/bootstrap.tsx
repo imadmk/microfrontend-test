ecdaf40ae85907748e01355403efa4714d2415f5
MK Imad <6089932+imadmk@users.noreply.github.com>
2025-11-06 14:58:09 +0700

    Add browser router as default

--- packages/marketing/src/bootstrap.tsx
+++ packages/marketing/src/bootstrap.tsx
@@ -1,15 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createMemoryHistory, createBrowserHistory, MemoryHistory, History } from 'history';
 
-const mount = (el: Element) => {
-  ReactDOM.render(<App />, el);
+interface MountOptions {
+  onNavigate?: (data: { pathname: string }) => void;
+  defaultHistory?: History;
+  initialPathName?: string;
+}
+
+const mount = (el: Element, options: MountOptions) => {
+  const { onNavigate, defaultHistory, initialPathName } = options;
+  const history = defaultHistory ?? createMemoryHistory({
+    initialEntries: [initialPathName],
+  });
+  if (onNavigate) {
+    history.listen((location) => {
+      onNavigate({ pathname: location.pathname });
+    });
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }: { pathname: string }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    }
+  }
 }
 
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, {
+      onNavigate: () => { },
+      defaultHistory: createBrowserHistory(),
+    });
   }
 }
 
